bca3c8b014c4860644a92637d517394ae9f14a02
Richard Sirokman <richds@gmail.com>
2023-12-21 14:13:20 -0600

    added a footer with year

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+// footer.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current year
+    var currentYear = new Date().getFullYear();
+  
+    // Set the current year in the span element
+    document.getElementById("currentYear").textContent = currentYear;
+  });
+  
